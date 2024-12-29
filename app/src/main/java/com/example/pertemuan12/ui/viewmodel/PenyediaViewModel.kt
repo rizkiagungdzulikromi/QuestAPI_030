f00db8055ea786f7d317001358c1c45d70d51c6c
rizkiagungdzulikromi <riskiagung3005@gmail.com>
2024-12-30 05:48:39 +0700

    tambah PenyediaViewModel untuk menyediakan ViewModel

---
+++ app/src/main/java/com/example/pertemuan12/ui/viewmodel/PenyediaViewModel.kt
@@ -0,0 +1,20 @@
+package com.example.pertemuan12.ui.viewmodel
+
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.createSavedStateHandle
+import androidx.lifecycle.viewmodel.CreationExtras
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
+import com.example.pertemuan12.MahasiswaApplications
+
+object PenyediaViewModel {
+    val Factory = viewModelFactory {
+        initializer { HomeViewModel(aplikasiKontak().container.kontakRepository) }
+        initializer { InsertViewModel(aplikasiKontak().container.kontakRepository) }
+        initializer { DetailViewModel(createSavedStateHandle(),aplikasiKontak().container.kontakRepository) }
+        initializer { UpdateViewModel(createSavedStateHandle(),aplikasiKontak().container.kontakRepository) }
+    }
+}
+
+fun CreationExtras.aplikasiKontak(): MahasiswaApplications =
+    (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as MahasiswaApplications)
